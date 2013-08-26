c0c43e4f3b2cd9b6dd1790626d46ec8c2f4ca1f2
Justin Cook <jcook@aea267.k12.ia.us>
2013-08-26 13:29:55 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vmerch::Application.config.secret_key_base = '3faa5241e0ddbc7d70e201199118c3363d2629077b1020f8cdd5dd645e2c43277331ae5c7c62e4023eabcd52f3bd45d7057223c64fc51b11644fc0f97e2e92c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vmerch::Application.config.session_store :cookie_store, key: '_vmerch_session'
