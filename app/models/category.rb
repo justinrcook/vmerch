af705e70edfb258ecfcaf089ffc0301eeb139c96
Justin Cook <jcook@aea267.k12.ia.us>
2013-09-12 00:16:03 -0500

    Shortcut methods for checking which layout a category is using.

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,9 @@ class Category < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  def single_column?; self.layout.eql?("single_column"); end
+  def two_column?; self.layout.eql?("two_column"); end
+
   def should_generate_new_friendly_id?
     name_changed?
   end
