a16480528ea257ce5321957e1c4deaceeb3f80fa
Justin Cook <jcook@aea267.k12.ia.us>
2013-09-23 16:27:13 -0500

    Validations.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,5 @@
 class Email < ActiveRecord::Base
+	validates_presence_of :name, :email, :message
+	validates_format_of :email, message: 'needs to be a valid email address',
+		with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
