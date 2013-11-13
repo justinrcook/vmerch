d861ec7d39ebb6da025c7c0d7dc0da1b04c7d1e8
Justin Cook <justinrcook@gmail.com>
2013-11-12 22:01:41 -0600

    Use the prodcution url.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ set :stage, :production
 
 # Server
 # ==================
-server 'beta.vmerch.me', roles: [:app, :web, :db]
+server 'vmerch.me', roles: [:app, :web, :db]
 set :deploy_to, '/home/justinrcook/websites/vmerch.me/www'
 set :shared_config_path, shared_path.join('config')
