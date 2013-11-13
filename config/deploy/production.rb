46efba577d08dff51a7ade76c044d9024c1ae47f
Justin Cook <justinrcook@gmail.com>
2013-11-12 21:52:38 -0600

    Updated to the actual production location.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :stage, :production
 # Server
 # ==================
 server 'beta.vmerch.me', roles: [:app, :web, :db]
-set :deploy_to, '/home/justinrcook/websites/vmerch.me/beta'
+set :deploy_to, '/home/justinrcook/websites/vmerch.me/www'
 set :shared_config_path, shared_path.join('config')
